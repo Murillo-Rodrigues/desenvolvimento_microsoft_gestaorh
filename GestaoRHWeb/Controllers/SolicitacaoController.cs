9c63385768593b637e2be4286dcfa4cd45ae5e50
Murillo Rodrigues <murillorodriguesdsouza@gmail.com>
2020-11-19 19:14:41 -0300

    Inclusão de Solicitação não finalizada

---
+++ GestaoRHWeb/Controllers/SolicitacaoController.cs
@@ -0,0 +1,20 @@
+﻿using GestaoRHWeb.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoRHWeb.Controllers
+{
+    public class SolicitacaoController : Controller
+    {
+
+        private readonly SolicitacaoDAO _solicitacaoDAO;
+
+        public SolicitacaoController(SolicitacaoDAO solicitacaoDAO) => _solicitacaoDAO = solicitacaoDAO;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
